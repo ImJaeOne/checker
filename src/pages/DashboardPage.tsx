09cee51d91e8b12366f311a84cca5505093c2f5c
ImJaeOne <dlawi0108@naver.com>
2025-06-30 18:09:59 +0900

    feat: 연차 사용 내역 UI #2

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,26 +1,26 @@
 import AdminAttendanceTable from '@/components/dashboard/AdminAttendanceTable';
 import AdminDashboardSummary from '@/components/dashboard/AdminDashboardSummary';
 import EmployeeDashboardSummary from '@/components/dashboard/EmployeeDashboardSummary';
-import EmployeeLeaveHistory from '@/components/dashboard/EmployeeLeaveHistory';
+import EmployeeLeaveTable from '@/components/dashboard/EmployeeLeaveTable';
 import { mockUsers, USER_ROLES } from '@/mocks/users';
 
 const DashboardPage = () => {
   const [employee, admin] = mockUsers;
 
-  if (admin.role === USER_ROLES.ADMIN) {
-    return (
-      <>
-        <AdminDashboardSummary />
-        <AdminAttendanceTable />
-      </>
-    );
-  }
+  // if (admin.role === USER_ROLES.ADMIN) {
+  //   return (
+  //     <>
+  //       <AdminDashboardSummary />
+  //       <AdminAttendanceTable />
+  //     </>
+  //   );
+  // }
 
   if (employee.role === USER_ROLES.EMPLOYEE) {
     return (
       <>
         <EmployeeDashboardSummary />
-        <EmployeeLeaveHistory />
+        <EmployeeLeaveTable />
       </>
     );
   }
