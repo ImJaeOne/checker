73d40be32db8182aeab037635d3ab6c0c5247faf
jinew <jiyunk@naver.com>
2025-06-30 17:53:34 +0900

    feat:#4 회원가입 폼 반영

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,11 +1,13 @@
+import RegisterForm from '@/components/register/registerForm';
 import { Link } from 'react-router-dom';
 
 const RegisterPage = () => {
   return (
-    <>
-      <div>RegisterPage</div>
+    <section>
+      <h2>회원가입</h2>
+      <RegisterForm />
       <Link to="/login">로그인</Link>
-    </>
+    </section>
   );
 };
 
