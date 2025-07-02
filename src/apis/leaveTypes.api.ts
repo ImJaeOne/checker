81f07a579845d0ecc9743900357f8e40b0d0810e
ImJaeOne <dlawi0108@naver.com>
2025-07-02 11:13:18 +0900

    feat: leave_types 테이블 데이터 조회 함수 getLeaveTypes 구현 #7

---
+++ src/apis/leaveTypes.api.ts
@@ -0,0 +1,13 @@
+import { supabase } from '@/utils/supabase';
+import type { LeaveTypesDTO } from '@/types/DTO/leaveTypes.dto';
+import { DB } from '@/constants/db.constant';
+
+export const getLeaveTypes = async (): Promise<LeaveTypesDTO[]> => {
+  const { data, error } = await supabase.from(DB.LEAVE_TYPES).select('*');
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return data;
+};
