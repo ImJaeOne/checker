324844e8af436b8d28f71e0345e28d812eeba938
jinew <jiyunk@naver.com>
2025-07-07 11:17:15 +0900

    chore:#4 return 타입 반영

--- src/apis/departments.api.ts
+++ src/apis/departments.api.ts
@@ -1,7 +1,8 @@
 import { supabase } from '@/utils/supabase';
 import { DB } from '@/constants/db.constant';
+import type { DepartmentsDTO } from '@/types/DTO/departments.dto';
 
-export const getDepartments = async () => {
+export const getDepartments = async (): Promise<DepartmentsDTO[]> => {
   const { data, error } = await supabase.from(DB.DEPARTMENTS).select('*');
 
   if (error) throw new Error(error.message);
--- src/apis/positions.api.ts
+++ src/apis/positions.api.ts
@@ -1,7 +1,8 @@
 import { supabase } from '@/utils/supabase';
 import { DB } from '@/constants/db.constant';
+import type { PositionsDTO } from '@/types/DTO/positions.dto';
 
-export const getPositions = async () => {
+export const getPositions = async (): Promise<PositionsDTO[]> => {
   const { data, error } = await supabase.from(DB.POSITIONS).select('*');
   if (error) throw new Error(error.message);
   return data;
