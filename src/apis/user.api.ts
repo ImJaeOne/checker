3a3f874ad595ea9f3e0a36d22141c57735de468a
jinew <jiyunk@naver.com>
2025-07-07 16:50:36 +0900

    chore:#14 리턴타입 수정

--- src/apis/user.api.ts
+++ src/apis/user.api.ts
@@ -1,11 +1,8 @@
 import { supabase } from '@/utils/supabase';
 import { DB } from '@/constants/db.constant';
-import type { PositionDTO } from '@/types/DTO/positions.dto';
-import type { UserDTO, UserId } from '@/types/DTO/user.dto';
+import type { UserId, UserWithPositionDTO } from '@/types/DTO/user.dto';
 
-export const getUser = async (
-  id: UserId,
-): Promise<UserDTO & { positions: PositionDTO }> => {
+export const getUser = async (id: UserId): Promise<UserWithPositionDTO> => {
   const { data, error } = await supabase
     .from(DB.USERS)
     .select('*, positions(id, name)')
