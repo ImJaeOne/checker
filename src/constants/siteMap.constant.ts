00a9e93c634fd91b27ceed00a57e43edc095dac5
ImJaeOne <dlawi0108@naver.com>
2025-07-07 16:36:08 +0900

    setting: shadcn sidebar 관련 라이브러리 설치 #9

--- src/constants/siteMap.constant.ts
+++ src/constants/siteMap.constant.ts
@@ -2,6 +2,10 @@ const SITE_MAP = {
   HOME: '/',
   LOGIN: '/login',
   REGISTER: '/register',
+  DASHBOARD: '/dashboard',
+  EMPLOYEES: '/employees',
+  ATTENDANCE: '/attendance',
+  LEAVE: '/leave',
 };
 
 export default SITE_MAP;
