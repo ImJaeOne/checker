b92a36c648be43ae666afa42f81f3cb2da3ef967
jinew <jiyunk@naver.com>
2025-07-07 11:59:31 +0900

    chore:#4 변경된 DTO 타입 반영

--- src/apis/departments.api.ts
+++ src/apis/departments.api.ts
@@ -2,7 +2,7 @@ import { supabase } from '@/utils/supabase';
 import { DB } from '@/constants/db.constant';
 import type { DepartmentsDTO } from '@/types/DTO/departments.dto';
 
-export const getDepartments = async (): Promise<DepartmentsDTO[]> => {
+export const getDepartments = async (): Promise<DepartmentsDTO> => {
   const { data, error } = await supabase.from(DB.DEPARTMENTS).select('*');
 
   if (error) throw new Error(error.message);
