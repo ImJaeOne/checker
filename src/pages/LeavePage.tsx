f469f8698f984b3cfd6d69dde1d3f0805a83a0b7
jinew <jiyunk@naver.com>
2025-07-09 13:58:12 +0900

    del:#19 기존 연차 페이지 삭제

--- src/pages/LeavePage.tsx
+++
@@ -1,9 +0,0 @@
-import { useGetLeaveTypesQuery } from '@/hooks/useLeaveTypesQuery';
-
-const LeavePage = () => {
-  const { data } = useGetLeaveTypesQuery();
-  console.log(data);
-  return <div>LeavePage</div>;
-};
-
-export default LeavePage;
