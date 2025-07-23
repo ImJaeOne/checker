4e4ecdcc3184184aa8394a3a17670ac6703d44c4
ImJaeOne <dlawi0108@naver.com>
2025-07-23 11:37:36 +0900

    add:#21 사용자 출/퇴근 기록 조회 useQuery 추가

---
+++ src/hooks/useAttendanceQuery.ts
@@ -0,0 +1,12 @@
+// hooks/useGetAttendanceQuery.ts
+import { getAttendance } from '@/apis/attendances.api';
+import { QUERY_KEY } from '@/constants/queryKey.constant';
+import { useQuery } from '@tanstack/react-query';
+
+export const useGetAttendanceQuery = (userId: string) => {
+  return useQuery({
+    queryKey: [QUERY_KEY.ATTENDANCE, userId],
+    queryFn: () => getAttendance(userId),
+    enabled: !!userId,
+  });
+};
